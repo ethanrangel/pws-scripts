02d2b3ad0061103e81034ce6dfca774339eb806f
Ethan Rangel <160991782+ethanrangel@users.noreply.github.com>
2024-12-03 13:01:30 -0500

    Update detectfonts.ps1

--- detectfonts.ps1
+++ detectfonts.ps1
@@ -1,24 +1,29 @@
-# List of font names as they appear in the registry
-$FontsToCheck = @(
-    "NotoSansJP-Black",
-    "NotoSansJP-Bold",
-    "NotoSansJP-ExtraBold",
-    "NotoSansJP-ExtraLight",
-    "NotoSansJP-Light",
-    "NotoSansJP-Medium",
-    "NotoSansJP-Regular",
-    "NotoSansJP-SemiBold",
-    "NotoSansJP-Thin",
-    "RobotoSlab-ExtraBold",
-    "RobotoSlab-Regular"
+####################################################
+# Script: detectfonts_v2.ps1
+# Scope: Detect if the specified font files exist in C:\Windows\Fonts
+####################################################
+
+# List of font file names (with extensions) to check in the Fonts directory
+$FontFilesToCheck = @(
+    "NotoSansJP-Black.ttf",
+    "NotoSansJP-Bold.ttf",
+    "NotoSansJP-ExtraBold.ttf",
+    "NotoSansJP-ExtraLight.ttf",
+    "NotoSansJP-Light.ttf",
+    "NotoSansJP-Medium.ttf",
+    "NotoSansJP-Regular.ttf",
+    "NotoSansJP-SemiBold.ttf",
+    "NotoSansJP-Thin.ttf",
+    "RobotoSlab-ExtraBold.ttf",
+    "RobotoSlab-Regular.ttf"
 )
 
 $AllFontsInstalled = $true
 
-foreach ($Font in $FontsToCheck) {
-    # Check if the font exists in the registry
-    if (-Not (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name $Font -ErrorAction SilentlyContinue)) {
-        Write-Host "Missing font: $Font"
+foreach ($FontFile in $FontFilesToCheck) {
+    # Check if the font file exists in C:\Windows\Fonts
+    if (-Not (Test-Path "C:\Windows\Fonts\$FontFile")) {
+        Write-Host "Missing font file: $FontFile"
         $AllFontsInstalled = $false
         break
     }
@@ -26,9 +31,10 @@ foreach ($Font in $FontsToCheck) {
 
 # Exit code based on detection result
 if ($AllFontsInstalled) {
-    Write-Host "All fonts are installed."
+    Write-Host "All font files are installed."
     exit 0
 } else {
-    Write-Host "Some fonts are missing."
+    Write-Host "Some font files are missing."
     exit 1
 }
+
