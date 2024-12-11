1d9856a85f5e64cfa974c30bfb7bfaec50dd6673
Ethan Rangel <160991782+ethanrangel@users.noreply.github.com>
2024-12-11 08:17:44 -0500

    Update installfonts.ps1

--- installfonts.ps1
+++ installfonts.ps1
@@ -1,20 +1,47 @@
-####################################################
-# Script: installfonts.ps1
-# Scope: Install fonts from the specified directory
-####################################################
-
-# Define the directory where the font files are located
-$FontDirectory = "C:\fontscripts\allfonts"
-
-# List all fonts (.TTF and .OTF) in the directory
-$Fonts = @(Get-ChildItem -Path "$FontDirectory\*.ttf").Name
-$Fonts += @(Get-ChildItem -Path "$FontDirectory\*.otf").Name
-
-# Register the fonts by copying them to the Fonts directory and updating the registry
-foreach ($Font in $Fonts) {
-    Copy-Item -Path "$FontDirectory\$Font" -Destination "$env:windir\Fonts" -Force
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name $Font -PropertyType String -Value $Font -Force
+function log() {
+    Param (
+        [string]$message
+    )
+    $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss tt"
+    Write-Output "$date - $message"
 }
 
-Write-Host "Fonts installed successfully on the device!"
-exit 0
+Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\fontInstall.log" -Force -Verbose
+
+# Get the current location and set the fonts folder dynamically
+$location = Get-Location
+$FontDirectory = "$location\newfonts" # Adjust to match where your fonts are stored
+
+# Get the fonts in the dynamic fonts folder
+$fonts = Get-ChildItem -Path "$FontDirectory"
+
+# Set the font REGPATH
+$regpath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
+
+# Set font values for each font
+foreach ($font in $fonts) {
+    $basename = $font.BaseName
+    $extension = $font.Extension
+    $fullname = $font.FullName
+    $fontname = $font.Name
+
+    if ($extension -eq ".ttf") {
+        $fontValue = "$basename (TrueType)"
+        log "Font value is $fontValue"
+    }
+
+    if ([string]::IsNullOrEmpty($fontValue)) {
+        log "Font not found or unsupported format"
+    } else {
+        if (Test-Path "$env:windir\Fonts\$fontname") {
+            log "Font $fontname already exists"
+        } else {
+            Copy-Item -Path $fullname -Destination "$env:windir\Fonts" -Force
+            log "Copied $fullname to C:\Windows\Fonts..."
+            reg.exe add $regpath /v "$fontValue" /t REG_SZ /d "$fontname" /f | Out-Host
+            log "Added $fontValue to registry"
+        }
+    }
+}
+
+Stop-Transcript
