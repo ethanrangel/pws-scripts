869add514202225113e945e4fab7472118532cef
Ethan Rangel <160991782+ethanrangel@users.noreply.github.com>
2024-12-11 08:18:37 -0500

    Update uninstallfonts.ps1

--- uninstallfonts.ps1
+++ uninstallfonts.ps1
@@ -1,25 +1,58 @@
-####################################################
-# Script: uninstallfonts.ps1
-# Scope: Uninstall fonts from the specified directory
-####################################################
+function log() {
+    Param (
+        [string]$message
+    )
+    $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss tt"
+    Write-Output "$date - $message"
+}
 
-# Define the directory where the font files are located
-$FontDirectory = "C:\fontscripts\allfonts"
+Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\fontUninstall.log" -Force -Verbose
 
-# List all fonts (.TTF and .OTF) in the directory
-$Fonts = @(Get-ChildItem -Path "$FontDirectory\*.ttf").Name
-$Fonts += @(Get-ChildItem -Path "$FontDirectory\*.otf").Name
+# Get the current location and set the fonts folder dynamically
+$location = Get-Location
+$FontDirectory = "$location\newfonts" # Adjust to match where your fonts are stored
 
-# Unregister the fonts by removing their files and registry entries
-foreach ($Font in $Fonts) {
-    try {
-        Remove-Item -Path "C:\Windows\Fonts\$Font" -Force
-        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name $Font -Force
-    } catch {
-        Write-Host "Error removing font: $Font"
-        Write-Host $_
+# Get the fonts in the dynamic fonts folder
+$fonts = Get-ChildItem -Path "$FontDirectory"
+
+# Set the font REGPATH
+$regpath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
+
+# Unregister and delete each font
+foreach ($font in $fonts) {
+    $basename = $font.BaseName
+    $extension = $font.Extension
+    $fontname = $font.Name
+
+    if ($extension -eq ".ttf") {
+        $fontValue = "$basename (TrueType)"
+        log "Font value to remove: $fontValue"
+    }
+
+    if ([string]::IsNullOrEmpty($fontValue)) {
+        log "Font not found or unsupported format"
+    } else {
+        # Remove the registry entry
+        try {
+            reg.exe delete $regpath /v "$fontValue" /f | Out-Host
+            log "Removed $fontValue from registry"
+        } catch {
+            log "Failed to remove $fontValue from registry"
+        }
+
+        # Remove the font file from Windows\Fonts
+        $fontPath = "$env:windir\Fonts\$fontname"
+        if (Test-Path $fontPath) {
+            try {
+                Remove-Item -Path $fontPath -Force
+                log "Removed $fontname from C:\Windows\Fonts"
+            } catch {
+                log "Failed to remove $fontname from C:\Windows\Fonts"
+            }
+        } else {
+            log "Font file $fontname not found in C:\Windows\Fonts"
+        }
     }
 }
 
-Write-Host "Fonts uninstalled successfully!"
-exit 0
+Stop-Transcript
